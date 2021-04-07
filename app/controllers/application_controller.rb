ba96eeecd19707622aef74a091b3912dd5d43259
Anton Lipich <anton.lipich1@gmail.com>
2021-04-07 19:05:57 +0300

    add Event policies and tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
@@ -19,5 +21,11 @@ class ApplicationController < ActionController::Base
     # если он есть, проверяем его юзера на равенство current_user.
     user_signed_in? &&
       (model.user == current_user || (model.try(:event).present? && model.event.user == current_user))
+  end
+
+  private
+  def user_not_authorized
+    flash[:alert] = t('pundit.not_authorized')
+    redirect_to(request.referrer || root_path)
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,32 +3,38 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   # Задаем объект @event для экшена show
-  before_action :set_event, only: [:show]
-
-  # Задаем объект @event от текущего юзера для других действий
-  before_action :set_current_user_event, only: [:edit, :update, :destroy]
+  before_action :set_event, except: [:index, :new, :create]
 
   before_action :password_guard!, only: [:show]
 
+  after_action :verify_authorized, only: [:edit, :update, :destroy, :show]
+  after_action :verify_policy_scoped, only: [:index]
+
   def index
-    @events = Event.all
+    @events = policy_scope(Event)
   end
 
   def show
+    authorize @event
     @new_comment =  @event.comments.build(params[:comment])
     @new_subscription = @event.subscriptions.build(params[:subscription])
     @new_photo =  @event.photos.build(params[:photo])
+
   end
 
   def new
     @event = current_user.events.build
+
+    authorize @event
   end
 
   def edit
+    authorize @event
   end
 
   def create
     @event = current_user.events.build(event_params)
+    authorize @event
 
     if @event.save
       redirect_to @event, notice: I18n.t('controllers.events.created')
@@ -38,6 +44,8 @@ class EventsController < ApplicationController
   end
 
   def update
+    authorize @event
+
     if @event.update(event_params)
       redirect_to @event, notice: I18n.t('controllers.events.updated')
     else
@@ -46,6 +54,8 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    authorize @event
+
     @event.destroy
     redirect_to events_url, notice: I18n.t('controllers.events.destroyed')
   end
@@ -63,10 +73,6 @@ class EventsController < ApplicationController
       flash.now[:alert] = I18n.t('controllers.events.wrong_pincode') if params[:pincode].present?
       render 'password_form'
      end
-  end
-
-  def set_current_user_event
-    @event = current_user.events.find(params[:id])
   end
 
   def set_event
