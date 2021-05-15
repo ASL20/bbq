97e46be0b9dc0c42715c0150cc7654a8239a14a7
Anton Lipich <anton.lipich1@gmail.com>
2021-05-15 22:53:45 +0300

    fix job

--- app/jobs/mail_notifications_job.rb
+++ app/jobs/mail_notifications_job.rb
@@ -1,16 +1,16 @@
 class MailNotificationsJob < ApplicationJob
   queue_as :default
 
-  def perform(type)
-    event = type.event
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [type.user&.email]).uniq
+  def perform(model)
+    event = model.event
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [model.user&.email]).uniq
 
-    if type.is_a?(Comment)
-      all_emails.each { |mail| EventMailer.comment(event, type, mail).deliver_later }
-    elsif type.is_a?(Photo)
-      all_emails.each { |mail| EventMailer.photo(event, type, mail).deliver_later }
-    else type.is_a?(Subscription)
-      EventMailer.subscription(event, type).deliver_later
+    if model.is_a?(Comment)
+      all_emails.each { |mail| EventMailer.comment(event, model, mail).deliver_later }
+    elsif model.is_a?(Photo)
+      all_emails.each { |mail| EventMailer.photo(event, model, mail).deliver_later }
+    else model.is_a?(Subscription)
+      EventMailer.subscription(event, model).deliver_later
     end
   end
 end
