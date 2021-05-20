9e76f092ee661c8958e708c222674275a7f254b0
Anton Lipich <anton.lipich1@gmail.com>
2021-05-20 11:01:16 +0300

    fix notifications job

--- app/jobs/mail_notifications_job.rb
+++ app/jobs/mail_notifications_job.rb
@@ -5,11 +5,12 @@ class MailNotificationsJob < ApplicationJob
     event = model.event
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [model.user&.email]).uniq
 
-    if model.is_a?(Comment)
+    case model
+    when Comment
       all_emails.each { |mail| EventMailer.comment(event, model, mail).deliver_later }
-    elsif model.is_a?(Photo)
+    when Photo
       all_emails.each { |mail| EventMailer.photo(event, model, mail).deliver_later }
-    else model.is_a?(Subscription)
+    when Subscription
       EventMailer.subscription(event, model).deliver_later
     end
   end
